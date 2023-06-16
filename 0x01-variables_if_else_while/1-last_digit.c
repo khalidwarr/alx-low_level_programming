767e7c3dab1cbb1a325a035ffe84b88de604f259
Abdessamad <Abdoubsabsa013@gmail.com>
2023-06-16 03:11:54 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,12 +5,14 @@
  *
  * Description: Prints the alphabet in lowercase,
  *              followed by a new line.
+ *              Prints a message based on the last digit of n.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch;
+	int n = 98;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
@@ -18,6 +20,13 @@ int main(void)
 	}
 
 	putchar('\n');
+
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
 	return (0);
 }
