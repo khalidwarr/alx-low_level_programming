9ddf49b291bf722aedcccb0f43d0d06bca2f5f06
Abdessamad <Abdoubsabsa013@gmail.com>
2023-06-17 04:02:48 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of four digits,
+ *              separated by ", " and in ascending order.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				for (d = c + 1; d <= 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					putchar(d + '0');
+
+					if (!(a == 9 && b == 8 && c == 7 && d == 6))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
